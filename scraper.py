e5ad14e36759358b102451fe46235feb045bf9ba
Quirin62 <91368114+Quirin62@users.noreply.github.com>
2025-04-15 14:45:21 +0200

    Bluesky post search :)

--- scraper.py
+++ scraper.py
@@ -1,7 +1,14 @@
+import sys, os
+os.chdir(os.path.dirname(sys.argv[0]))
+
 from atproto import Client
 
 client = Client()
-client.login("", "")
+with open("login.txt", "r") as login:
+    creds = login.read().split("\n")
+    #print(creds)
+    client.login(creds[0], creds[1])
 
-
+resp = client.app.bsky.feed.search_posts(params={"q":"Book"})
+print(resp)
 
