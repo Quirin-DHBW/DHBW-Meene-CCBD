d942d82831ee79ba6a9a3b2e673122d922b84533
Quirin62 <91368114+Quirin62@users.noreply.github.com>
2025-04-29 15:27:28 +0200

    Parsing vervollständigt, jetzt noch einfüttern in DB und dann analysieren! (pull.txt ist eine test datei, damit wir nicht jedes mal einen Pull beim testen machen müssen)

--- scraper.py
+++ scraper.py
@@ -1,7 +1,10 @@
 import sys, os
 os.chdir(os.path.dirname(sys.argv[0]))
 
+# The official Bluesky API accessing package
 from atproto import Client
+
+import duckdb
 
 client = Client()
 with open("login.txt", "r") as login:
@@ -9,6 +12,20 @@ with open("login.txt", "r") as login:
     #print(creds)
     client.login(creds[0], creds[1])
 
-resp = client.app.bsky.feed.search_posts(params={"q":"Book"})
+with duckdb.connect("bsky_posts.db") as db:
+    pass
+
+
+# Stop this to prevent unnecessary scraping for now (don't need to use our limited daily API calls if I have a test set saved UwU)
+#"""
+resp = client.app.bsky.feed.search_posts(params={"q":"Book", "limit":100})
+with open("pull.txt", "w", encoding="utf-8") as f:
+    print(resp)
+    for post in resp.posts:
+        cleaned_post_text = post.record.text.replace("\n", " <b> ")
+        f.write(cleaned_post_text + "\n")
 print(resp)
+#"""
+
+
 
