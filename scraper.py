e2631bd133ec5c0d5cd291cc846839c951dc0091
Schneefelsen <tim@saonet.de>
2025-04-30 14:12:42 +0200

    code now creates and writes scraped data directly into a duckdb database.

--- scraper.py
+++ scraper.py
@@ -13,7 +13,36 @@ with open("login.txt", "r") as login:
     client.login(creds[0], creds[1])
 
 with duckdb.connect("bsky_posts.db") as db:
-    pass
+    db.execute("""
+        CREATE TABLE IF NOT EXISTS posts (
+            timestamp TIMESTAMPZ,
+            text TEXT,
+            uri TEXT,
+            like_count INTEGER,
+            quote_count INTEGER,
+            reply_count INTEGER,
+            repost_count INTEGER
+        )
+    """)
+
+    # This part is for directly writing it into the db
+    resp = client.app.bsky.feed.search_posts(params={"q":"Trump", "limit":100})
+    for post in resp.posts:
+        try:
+            timestamp = post.record.created_at
+            text = post.record.text
+            uri = post.uri
+            like_count = post.like_count
+            quote_count = post.quote_count
+            reply_count = post.reply_count
+            repost_count = post.repost_count
+
+            db.execute("""
+                INSERT INTO posts (timestamp, text, uri, like_count, quote_count, reply_count, repost_count)
+                VALUES (?, ?, ?, ?, ?, ?, ?)
+            """, (timestamp, text, uri, like_count, quote_count, reply_count, repost_count))
+        except Exception as e:
+            print(f"Fehler beim Verarbeiten eines Posts: {e}")
 
 # Stop this to prevent unnecessary scraping for now (don't need to use our limited daily API calls if I have a test set saved UwU)
 """
